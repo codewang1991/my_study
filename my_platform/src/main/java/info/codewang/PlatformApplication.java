fcef5815d8de837f544e9a09fbb18a4a2619aadb
wangmin_kimed <1076953055@qq.com>
2018-02-06 15:06:02 +0800

    增加springboot配置

--- my_platform/src/main/java/info/codewang/PlatformApplication.java
+++ my_platform/src/main/java/info/codewang/PlatformApplication.java
@@ -1,5 +1,21 @@
 package info.codewang;
 
-public class PlatformApplication {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@SpringBootApplication
+@RestController
+public class PlatformApplication {
+    
+    
+    @GetMapping(value="/hello")
+    public String hello(){
+	return "你好";
+    }
+    
+    public static void main(String[] args) {
+	SpringApplication.run(PlatformApplication.class, args);
+    }
 }
